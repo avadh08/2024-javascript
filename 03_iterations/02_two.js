b9dfb9a88ef854d7969e8a7dfb02f6a7c34b7145
avadh08 <155340441+avadh08@users.noreply.github.com>
2024-01-25 11:26:17 +0000

    update

---
+++ 03_iterations/02_two.js
@@ -0,0 +1,25 @@
+// while loop.
+
+let index = 0
+while (index < 11) {
+    console.log(index)
+    index++
+}
+
+let myArray = ["batman", "superman", "flash"]
+
+let i = 0
+
+while (i < 3) {
+    console.log(`my hero is : ${myArray[i]}`)
+    i++
+}
+
+// do while loop.
+
+let score = 0
+
+do{
+    console.log(score)
+    score++
+}while(score < 11);
