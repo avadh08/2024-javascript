975f8953a00908bba4d313a6134341c5bc7d2281
avadh08 <155340441+avadh08@users.noreply.github.com>
2024-01-25 13:04:24 +0000

    update

---
+++ 03_iterations/06_six.js
@@ -0,0 +1,13 @@
+const name = ["avadh", "mira", "alpesh", "asmita"]
+
+const n1 = name.forEach( (item) => { 
+      console.log(item)
+      return item
+})
+
+console.log(n1)
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const num = myNums.filter( (item) => item > 5)
+console.log(num)
