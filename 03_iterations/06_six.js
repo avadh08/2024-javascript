8cc4c3a825c97c5f9a08f2d86559e869ff9e1808
avadh08 <155340441+avadh08@users.noreply.github.com>
2024-01-28 07:10:26 +0000

    update

--- 03_iterations/06_six.js
+++ 03_iterations/06_six.js
@@ -9,5 +9,20 @@ console.log(n1)
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const num = myNums.filter( (item) => item > 5)
-console.log(num)
+// const num = myNums.filter( (item) => item > 5)
+// console.log(num)
+
+// const num = myNums.filter( (item) => {
+//       return item > 5
+// })
+// console.log(num);
+
+const newNums = []
+
+myNums.forEach( (num) => {
+      if (num > 5) {
+            newNums.push(num)
+      }
+})
+
+console.log(newNums)
